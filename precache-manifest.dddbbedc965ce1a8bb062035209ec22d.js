4391ebe975d066a499b296a35c19319df56113bd
CTACON23 <stanyslavyavor23@gmail.com>
2020-03-12 12:17:07 +0200

    Updates

---
+++ precache-manifest.dddbbedc965ce1a8bb062035209ec22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c31b5f32c60bbe1df3d0f805452d17f",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "8a1125d00373a4e533a5",
+    "url": "/todoList/static/css/main.a0b8ff43.chunk.css"
+  },
+  {
+    "revision": "70a907b85ece3f24aa59",
+    "url": "/todoList/static/js/2.fbcffefa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todoList/static/js/2.fbcffefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1125d00373a4e533a5",
+    "url": "/todoList/static/js/main.d00f5930.chunk.js"
+  },
+  {
+    "revision": "3eaaaa72a3e648ad7b58",
+    "url": "/todoList/static/js/runtime-main.e7695603.js"
+  }
+]);
